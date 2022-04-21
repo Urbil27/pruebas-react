9f3c3536453cc35e1a78bcb92ae2a8fd4ac75788
Urbil Melin <urbiltxo@gmail.com>
2022-04-21 10:32:10 +0200

    Hola mundo hecho

--- prueba-react/src/App.js
+++ prueba-react/src/App.js
@@ -2,24 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombre = prompt("Escribe tu nombre:")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      hola(nombre)
+
   );
 }
+function hola(nom){
 
+
+  let contenido = <h1>Hola {nom}</h1>;
+  return (contenido);
+}
 export default App;
